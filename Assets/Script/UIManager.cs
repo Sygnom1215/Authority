a3c8543dd5b1779bb56d80d5efb246652375c550
DESKTOP-OHD6DA9\USER <seoyun0700@daum.net>
2021-11-03 23:46:57 +0900

    text 시작

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<Text> texts = new List<Text>();
+    [SerializeField]
+    private List<float> textX = new List<float>();
+    [SerializeField]
+    private List<float> textY = new List<float>();
     /* 해야 하는 거 
      * pause << 당장에 만들 수 있는거 ??
      * Scene Out 
